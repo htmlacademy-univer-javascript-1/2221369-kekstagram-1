9b9f95377c0d5e04bc6087bc82a541cb688b7c2b
diananublin <113102970+diananublin@users.noreply.github.com>
2022-11-30 02:26:30 +0500

    Создание массива из 25 сгенерированных объектов

--- js/main.js
+++ js/main.js
@@ -31,19 +31,19 @@ const checkMaxLenghtString = (string, limit) => string.lenght <= limit;
 checkMaxLenghtString('бла-бла', 30);
 
 const createComment = (id) => ({
-    id,
-    avatar: 'img/avatar-${getRandomPositiveInteger(1, 6)}.svg',
-    message: MESSAGES[getRandomPositiveInteger(0, MESSAGES.length - 1)],
-    name: NAMES[getRandomPositiveInteger(0, NAMES.length - 1)],
-  });
+  id,
+  avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+  message: MESSAGES[getRandomPositiveInteger(0, MESSAGES.length - 1)],
+  name: NAMES[getRandomPositiveInteger(0, NAMES.length - 1)],
+});
 
 const createPhoto = (id) => ({
-    id,
-    url: 'photos/${id}.jpg',
-    description: DESCRIPTIONS[getRandomPositiveInteger(0, DESCRIPTIONS.length - 1)],
-    likes: getRandomPositiveInteger(CountLike.MIN, CountLike.MAX),
-    comments: Array.from({length: getRandomPositiveInteger(1, MAX_COUNT_COMMENTS)}, createComment),
-  });
+  id,
+  url: `photos/${id}.jpg`,
+  description: DESCRIPTIONS[getRandomPositiveInteger(0, DESCRIPTIONS.length - 1)],
+  likes: getRandomPositiveInteger(CountLike.MIN, CountLike.MAX),
+  comments: Array.from({length: getRandomPositiveInteger(1, MAX_COUNT_COMMENTS)}, createComment),
+});
 const photosDescriptions = Array.from({length: MAX_COUNT_PHOTOS}, createPhoto);
 
 console.log(photosDescriptions);
