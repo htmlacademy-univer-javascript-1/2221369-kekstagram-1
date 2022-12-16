bc9415f670dd9533d7998daf4406fbef7d57f3b3
diananublin <113102970+diananublin@users.noreply.github.com>
2022-12-16 08:00:15 +0500

    реализация загрузки по 5 комментариев под фотографией

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,32 +1,52 @@
 import { isEscapeKey } from './utils.js';
+import { DEFAULT_RENDERED_COMMENTS } from './consts.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const commentsList = bigPicture.querySelector('.social__comments');
 const counterComments = bigPicture.querySelector('.social__comment-count');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
 const commentsLoaderButton = bigPicture.querySelector('.comments-loader');
-const commentsCount = bigPicture.querySelector('.comments-count');
 
 let actualComments = [];
+let countRenderedComments = DEFAULT_RENDERED_COMMENTS;
 
 const getCommentTemplate = ({avatar, message, name}) => `<li class="social__comment">
   <img class="social__picture" src="${avatar}" alt="${name}" width="35" height="35">
   <p class="social__text">${message}</p>
 </li>`;
 
-const renderComments = (commentsLength) => {
-  commentsCount.textContent = commentsLength;
+const getCounterCommentsTemplate = (commentsCount) => `${Math.min(countRenderedComments, commentsCount)} из <span class="comments-count">${commentsCount}</span> комментариев`;
+
+const getCounterComments = () => {
+  counterComments.innerHTML = '';
+  counterComments.insertAdjacentHTML('afterbegin', getCounterCommentsTemplate(actualComments.length));
+};
+
+const renderComments = () => {
+  getCounterComments();
+
   commentsList.innerHTML = '';
-  const commentsTemplate = actualComments.slice().map((comment) => getCommentTemplate(comment)).join('');
+  const commentsTemplate = actualComments.slice(0, countRenderedComments).map((comment) => getCommentTemplate(comment)).join('');
   commentsList.insertAdjacentHTML('afterbegin', commentsTemplate);
+
+  if (countRenderedComments >= actualComments.length) {
+    commentsLoaderButton.removeEventListener('click', onCommentsLoaderButtonClick);
+    commentsLoaderButton.classList.add('hidden');
+  }
 };
 
 const initComments = (comments) => {
   actualComments = comments.slice();
   commentsList.innerHTML = '';
-  counterComments.classList.add('hidden');
-  commentsLoaderButton.classList.add('hidden');
-  renderComments(comments.length);
+
+  if (comments.length === 0) {
+    commentsLoaderButton.classList.add('hidden');
+    counterComments.textContent = 'Нет комментариев';
+    return;
+  }
+
+  renderComments();
+  commentsLoaderButton.addEventListener('click', onCommentsLoaderButtonClick);
 };
 
 const renderBigPicture = (picture) => {
@@ -41,6 +61,9 @@ const closeBigPicture = () => {
   bigPicture.classList.add('hidden');
   closeButton.removeEventListener('click', onCloseButtonClick);
   document.removeEventListener('keydown', onWindowEscKeyDown);
+  commentsLoaderButton.classList.remove('hidden');
+  commentsLoaderButton.removeEventListener('click', onCommentsLoaderButtonClick);
+  countRenderedComments = DEFAULT_RENDERED_COMMENTS;
 };
 
 function onCloseButtonClick() {
@@ -51,6 +74,11 @@ function onWindowEscKeyDown(evt) {
   if (isEscapeKey(evt)) {
     closeBigPicture();
   }
+}
+
+function onCommentsLoaderButtonClick() {
+  countRenderedComments += DEFAULT_RENDERED_COMMENTS;
+  renderComments();
 }
 
 const openBigPicture = (photo) => {
