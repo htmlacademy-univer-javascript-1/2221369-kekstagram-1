fd992476305cc6d12f9336e234ba1432d2a4cead
diananublin <113102970+diananublin@users.noreply.github.com>
2022-12-20 05:45:12 +0500

    реализация редактирования масштаба изображения и применения эффекта

--- js/form.js
+++ js/form.js
@@ -1,5 +1,7 @@
 import { isEscapeKey } from './utils.js';
 import { form, showSubmitButton, validateForm } from './validation-form.js';
+import { onScaleButtonClick, scaleContainer } from './picture-scale.js';
+import { effectList, addEffect, hideUiSlider } from './effects.js';
 
 const body = document.querySelector('body');
 const imgUploadField = document.querySelector('#upload-file');
@@ -7,24 +9,29 @@ const editImgField = document.querySelector('.img-upload__overlay');
 const closeButton = form.querySelector('.img-upload__cancel');
 const hashtagsField = form.querySelector('.text__hashtags');
 const commentsField = form.querySelector('.text__description');
+const imgUploadPreview = document.querySelector('.img-upload__preview img');
 
 const closeUploadPopup  = () => {
   editImgField.classList.add('hidden');
   body.classList.remove('modal-open');
+  scaleContainer.removeEventListener('click', onScaleButtonClick);
+  effectList.removeEventListener('click', addEffect);
+  document.removeEventListener('keydown', onButtonEscKeydown);
+  closeButton.removeEventListener('click', onCloseButtonClick);
+  imgUploadPreview.removeAttribute('class');
+  imgUploadPreview.removeAttribute('style');
   form.reset();
 };
 
-const onButtonEscKeydown = (evt) => {
+function onButtonEscKeydown(evt) {
   if (isEscapeKey(evt)) {
     closeUploadPopup();
-    document.removeEventListener('keydown', onButtonEscKeydown);
   }
-};
+}
 
-const onCloseButtonClick = () => {
+function onCloseButtonClick() {
   closeUploadPopup();
-  document.removeEventListener('keydown', onButtonEscKeydown);
-};
+}
 
 const setFocusListeners = (field) => {
   field.addEventListener('focus', () => {
@@ -40,6 +47,9 @@ const onImgUploadFieldChange = () => {
   body.classList.add('modal-open');
   closeButton.addEventListener('click', onCloseButtonClick);
   document.addEventListener('keydown',onButtonEscKeydown);
+  scaleContainer.addEventListener('click', onScaleButtonClick);
+  hideUiSlider();
+  effectList.addEventListener('click', addEffect);
   setFocusListeners(commentsField);
   setFocusListeners(hashtagsField);
   showSubmitButton();
@@ -55,4 +65,4 @@ const renderUploadForm = () => {
   validateForm();
 };
 
-export { renderUploadForm, hashtagsField, commentsField};
+export { renderUploadForm, hashtagsField, commentsField, imgUploadPreview };
